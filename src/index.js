d14da87fd372a26e6cbfb2744ce10e335ad5b40d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2022-01-13 13:58:05 +0200

    Add package stuff ported from wise.theme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,37 @@
-const applyConfig = (config) => {
-  return config;
+import React from 'react';
+import { mergeConfig } from '@eeacms/search';
+import { wise_config, wise_resolve } from './config';
+
+const getClientProxyAddress = () => {
+  const url = new URL(window.location);
+  url.pathname = '';
+  url.search = '';
+  return url.toString();
 };
 
-export default applyConfig;
+export function installMeasuresCatalogue(config) {
+  const pjson = require('../package.json');
+
+  const registry = config.settings.searchlib;
+  const envConfig = mergeConfig(wise_config, registry.searchui.default);
+  registry.searchui.wise = envConfig;
+  registry.resolve = mergeConfig(wise_resolve, registry.resolve);
+  registry.searchui.wise.resultViews[0].icon = 'list';
+  registry.searchui.wise.resultViews[1].icon = 'table';
+
+  envConfig.app_name = pjson.name;
+  envConfig.app_version = pjson.version;
+
+  if (typeof window !== 'undefined' && process.env.USE_ES_PROXY === 'true') {
+    config.searchui.wise.host =
+      process.env.RAZZLE_ES_PROXY_ADDR || getClientProxyAddress();
+  }
+
+  console.log('config', config.settings.searchlib);
+
+  return config;
+}
+
+export default function install(config) {
+  return config;
+}
