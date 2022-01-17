d70b9892a5323d5c3702152887089bbbe042a577
GhitaB <ghitabizau27@gmail.com>
2022-01-17 15:33:13 +0200

    Refs #143927 - Listing Item.

--- src/components/ListingViewItem.jsx
+++ src/components/ListingViewItem.jsx
@@ -34,52 +34,48 @@ const Inner = (props) => {
 
   console.log(result);
   return (
-    <p>{result.testtest}</p>
+    <Item>
+      <Item.Content>
+        <Header {...props} {...listingViewParams} appConfig={appConfig} />
+        <Item.Extra>
+          {listingViewParams?.extraFields?.map(({ field, label }, i) =>
+            field === 'Descriptors' ? (
+              <div className="simple-item-extra descriptor-item" key={i}>
+                <span className="simple-item-label">{label}:</span>{' '}
+                {typeof result[field]?.raw === 'string' ? (
+                  <DescriptorItem
+                    facets={facets}
+                    descriptor={result[field]?.raw}
+                  />
+                ) : Array.isArray(result[field]?.raw) ? (
+                  result[field]?.raw.map((item, i) => (
+                    <span key={i} className="array-string-item">
+                      {i > 0 && ', '}
+                      <DescriptorItem
+                        key={i}
+                        facets={facets}
+                        descriptor={item}
+                      />
+                    </span>
+                  ))
+                ) : (
+                  <DescriptorItem
+                    facets={facets}
+                    descriptor={result[field]?.raw}
+                  />
+                )}
+              </div>
+            ) : (
+              <div className="simple-item-extra" key={i}>
+                <span className="simple-item-label">{label}:</span>{' '}
+                <String val={result[field]?.raw} />
+              </div>
+            ),
+          )}
+        </Item.Extra>
+      </Item.Content>
+    </Item>
   );
-
-  // return (
-  //   <Item>
-  //     <Item.Content>
-  //       <Header {...props} {...listingViewParams} appConfig={appConfig} />
-  //       <Item.Extra>
-  //         {listingViewParams?.extraFields?.map(({ field, label }, i) =>
-  //           field === 'Descriptors' ? (
-  //             <div className="simple-item-extra descriptor-item" key={i}>
-  //               <span className="simple-item-label">{label}:</span>{' '}
-  //               {typeof result[field]?.raw === 'string' ? (
-  //                 <DescriptorItem
-  //                   facets={facets}
-  //                   descriptor={result[field]?.raw}
-  //                 />
-  //               ) : Array.isArray(result[field]?.raw) ? (
-  //                 result[field]?.raw.map((item, i) => (
-  //                   <span key={i} className="array-string-item">
-  //                     {i > 0 && ', '}
-  //                     <DescriptorItem
-  //                       key={i}
-  //                       facets={facets}
-  //                       descriptor={item}
-  //                     />
-  //                   </span>
-  //                 ))
-  //               ) : (
-  //                 <DescriptorItem
-  //                   facets={facets}
-  //                   descriptor={result[field]?.raw}
-  //                 />
-  //               )}
-  //             </div>
-  //           ) : (
-  //             <div className="simple-item-extra" key={i}>
-  //               <span className="simple-item-label">{label}:</span>{' '}
-  //               <String val={result[field]?.raw} />
-  //             </div>
-  //           ),
-  //         )}
-  //       </Item.Extra>
-  //     </Item.Content>
-  //   </Item>
-  // );
 };
 
 const ResultItem = (props) => {
