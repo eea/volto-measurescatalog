819d71879028ed7d89023c545e607b4edc56af20
GhitaB <ghitabizau27@gmail.com>
2022-01-21 15:50:45 +0200

    Refs #143927 - WIP layout.

--- src/components/WiseLayout.jsx
+++ src/components/WiseLayout.jsx
@@ -7,14 +7,30 @@ const RightColumnLayout = (props) => {
   const { bodyContent, bodyFooter, bodyHeader, header, sideContent } = props;
   return (
     <div id="search-app">
-      <Grid columns={2} stackable className="body-content">
+      <Grid className="body-header">
         <Grid.Row>
           <Grid.Column widescreen={12}>
             <div>{header}</div>
             <div>{bodyHeader}</div>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <Grid columns={2} stackable className="body-content">
+        <Grid.Row>
+          <Grid.Column
+            widescreen="10"
+            tablet="10"
+            computer="10"
+            className="col-left"
+          >
             <div>{bodyContent}</div>
           </Grid.Column>
-          <Grid.Column widescreen="4">
+          <Grid.Column
+            widescreen="2"
+            tablet="2"
+            computer="2"
+            className="col-right"
+          >
             <h3>Filter by</h3>
             <div>{sideContent}</div>
           </Grid.Column>
