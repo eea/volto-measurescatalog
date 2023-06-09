9374dfb6e30e659377d8517a74030f85ccbdd828
valentinab25 <valentinab25>
2023-06-09 20:35:53 +0300

    test: jest should look for addons in node_modules Refs #253277

--- jest-addon.config.js
+++ jest-addon.config.js
@@ -12,7 +12,7 @@ module.exports = {
     '@package/(.*)$': '<rootDir>/src/$1',
     '@plone/volto-quanta/(.*)$': '<rootDir>/src/addons/volto-quanta/src/$1',
     '@root/(.*)$': '<rootDir>/src/$1',
-    '@eeacms/(.*?)/(.*)$': '<rootDir>/src/addons/$1/src/$2',
+    '@eeacms/(.*?)/(.*)$': '<rootDir>/node_modules/@eeacms/$1/src/$2',
     'volto-slate/(.*)$': '<rootDir>/src/addons/volto-slate/src/$1',
     '~/(.*)$': '<rootDir>/src/$1',
     'load-volto-addons':
